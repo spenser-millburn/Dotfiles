5526e6c182f8091b24beef931e6474d7492e22f8
spenser <millburn.s@northeastern.edu>
2022-04-19 01:38:24 -0400

    [FIX] bad fish path in .config folder

--- essentials_fish.sh
+++ essentials_fish.sh
@@ -21,6 +21,7 @@ curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo 
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
 sudo apt update
 sudo apt install gh
+sudo cp $HOME/Dotfiles/ubuntu/chsh /etc/pam.d/chsh
 # chsh -s $(which zsh)
 cp $HOME/Dotfiles/tmux/.tmux.conf $HOME
 rm -rf $HOME/.config/fish
