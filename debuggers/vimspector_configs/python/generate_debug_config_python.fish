40bb78d1b829396e1703f4666334118532394a39
Spenser Millburn <spenser.millburn@alertinnovation.com>
2022-12-17 01:24:11 -0500

    [ADD] Vimspector debugger configs for python and c++

---
+++ debuggers/vimspector_configs/python/generate_debug_config_python.fish
@@ -0,0 +1,4 @@
+function generate_debug_config_python
+cp $HOME/Dotfiles/debuggers/vimspector_configs/python/.vimspector.json .
+cp $HOME/Dotfiles/debuggers/vimspector_configs/python/run_with_debugpy . 
+end
