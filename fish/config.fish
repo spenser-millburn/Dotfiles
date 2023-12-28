5bd01b39892e70259042d45414c24f9b9881418e
mclovin <ceo@sliink.co>
2023-12-27 21:04:38 -0500

    [UPDATE] fish config and install script

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,23 @@
 if status is-interactive
-set -x EDITOR nvim
-and not set -q TMUX
-    exec tmux
 end
 
+#Editor
+set -x EDITOR nvim
 
+alias kc="kubectl"
+alias v="vim"
+alias top="htop"
+                                                                                             
+#OPENAI
+set -x OPENAI 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
+
+#gpt-engineer
+set -x OPENAI_API_KEY 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
+
+set -x SLINK_FIREBASE_KEY AIzaSyDP3zjsVWdyqOlHQJ1KE5gz9FKuGfGHWI8
+
+#Alpaca
+set -x ALPACA_API_KEY PKRZ0FNHUSRK5HEPT522
+set -x ALPACA_API_SECRET TXoR24ehOjpT4L9XxMpY12fBrugZPwCYI1Ma0Gt2
+
+bass eval (keychain --eval --agents ssh -Q --quiet id_ed25519 --nogui)
